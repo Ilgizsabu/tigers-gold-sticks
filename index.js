cc754ac14c59e886123348e80e21f4a0af1c0c5b
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-07 21:38:44 +0300

    add cod

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const sticks = alert('Всего 20 палочек');
-const user = +prompt('Выберете 1 палочку или 2 палочки');
+let stiksBalance = 20;
 
-if (user > 2) {
-    alert(`Осталось ${result} палочек`);
+while (stiksBalance > 1) {
+    const humanStiks = +prompt(`Всего ${stiksBalance} палочек, выберите сколько: 1, 2 или 3`);
+
+    if (humanStiks < 1 || humanStiks > 3 || isNaN(humanStiks)) {
+        alert('Некорректный ввод. Выберите число от 1 до 3.');
+        continue;
+    }
+
+    stiksBalance -= humanStiks;
+
+    if (stiksBalance <= 0) {
+        alert('Вы проиграли! Компьютер выиграл.');
+        break;
+    }
+
+    alert(`Отлично, осталось ${stiksBalance} палочек, далее ход компьютера!`);
+
+    const computerProgress = Math.floor(Math.random() * 3) + 1;
+    stiksBalance -= computerProgress;
+
+    if (stiksBalance <= 0) {
+        alert('Компьютер проиграл!');
+        break;
+    }
+
+    alert(`Компьютер выбрал ${computerProgress}, осталось ${stiksBalance} палочек, далее ваш ход!`);
 }
